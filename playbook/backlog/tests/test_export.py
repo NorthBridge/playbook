643edd73718725e2b744af38c5fd38d881911ba6
Bruno Godoy Martins Correa <bcorrea@luc.edu>
2015-06-12 16:30:26 -0300

    Tests for the export process

---
+++ playbook/backlog/tests/test_export.py
@@ -0,0 +1,12 @@
+from .. import export
+import unittest
+
+class ExportTest(unittest.TestCase):
+    
+    def test(self):
+        #Set up the environment for the test
+        #invoke the export script
+        #export.main()
+        #check if all stuff were done correctly
+        #Clean up the environment after the test
+        pass
