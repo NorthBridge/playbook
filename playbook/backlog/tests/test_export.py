ca06abcd0bfcaf998892f2f25342dea523a6bc71
L. Blake Wilson <blake.wilson@northbridgetech.org>
2015-06-13 20:37:06 -0500

    test_export removes test milestone from database

--- playbook/backlog/tests/test_export.py
+++ playbook/backlog/tests/test_export.py
@@ -5,13 +5,16 @@ from ..dao.milestoneDAO import MilestoneDAO
 from ..model.milestone import Milestone
 from ..model.issue import Issue
 import psycopg2.extras
+from pygithub3 import Github
+from ...utils.configHelper import getConfig
 
 class ExportTest(unittest.TestCase):
 
     def environ_setup(self):
-        #update database with test data
+        # update database with test data
 
-        backlog_stmt = """INSERT INTO backlog (
+        # create test row for export in backlog
+        backlog_create_row = """INSERT INTO backlog (
             id,
             story_title,
             story_descr,
@@ -21,13 +24,31 @@ class ExportTest(unittest.TestCase):
             'This milestone was created by the test_export module',
             14);"""
         bDAO = BaseDAO()
-        cur = bDAO.getConnection().cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute(backlog_stmt,)
+        cur = bDAO.getConnection().cursor(
+                                      cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(backlog_create_row,)
         bDAO.getConnection().commit()
         cur.close()
 
     def environ_cleanup(self):
-        pass
+        # remove test data from backlog table
+        backlog_delete_row = """DELETE FROM backlog WHERE 
+                                story_title = 'TEST';"""
+        bDAO = BaseDAO()
+        cur = bDAO.getConnection().cursor(
+                                      cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute(backlog_delete_row,)
+        bDAO.getConnection().commit()
+        cur.close()
+
+    def github_remove(self):
+        # in order to remove Milestone, need Milestone number (int)
+        token = getConfig("github.token")
+        owner = getConfig("github.owner")
+        gh = Github(token=token, user=owner, repo='test-community')
+        northbridge = gh.users.get('northbridge')
+        response = gh.issues.milestones.list(owner, 'test-community')
+        print response.all()
     
     def test(self):
         #Set up the environment for the test
@@ -43,7 +64,8 @@ class ExportTest(unittest.TestCase):
         # i.e. remove test milestone and related issues from database
         # then remove milestone and related issues from GitHub
         self.environ_cleanup()
-        pass
 
+        # self.github_remove()
+        
 if __name__ == '__main__':
     unittest.main()
